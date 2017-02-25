e42bb8de59149bf3e77de8af6fc1f3c07a864fc3
Charles Painter <cpainter85@gmail.com>
2017-02-25 17:35:18 -0500

    16: adds onInputChange event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 // Every class based component needs a defined render method
 class SearchBar extends Component {
   render() {
-    return <input />;
+    return <input onChange={this.onInputChange} />;
+  }
+
+  // Event handler
+  onInputChange(event) {
+    console.log(event.target.value);
   }
 }
 
