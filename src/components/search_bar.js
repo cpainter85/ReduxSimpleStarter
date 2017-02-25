4a1e67bdef5e7b891302f7ff368e1aebe7e99161
Charles Painter <cpainter85@gmail.com>
2017-02-25 17:53:13 -0500

    17: Introduction to state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 // Every class based component needs a defined render method
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() {
     return <input onChange={event => console.log(event.target.value)} />;
   }
