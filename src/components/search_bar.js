08be1db6ec51e8406640a3c0ab82430f1b3edaf3
Charles Painter <cpainter85@gmail.com>
2017-02-26 17:23:17 -0500

    31: Adds videoSearch

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,14 +9,18 @@ class SearchBar extends Component {
   }
 
   render() {
-    // Always manipulate state with setState
     return (
       <div className="search-bar">
         <input
           value={this.state.term}
-          onChange={event => this.setState({ term: event.target.value })} />
+          onChange={event => this.onInputChange(event.target.value)} />
       </div>
     );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
