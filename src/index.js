0a96ee14b56d22af6a257531c94006a3a296f220
Charles Painter <cpainter@eenews.net>
2017-08-25 15:47:33 -0400

    Revises App to class component
    Lesson 22

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import SearchBar from './components/search_bar';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
