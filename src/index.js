117c4bb28265972cea77987783a590ded9c6dda6
Charles Painter <cpainter@eenews.net>
2017-08-25 16:10:00 -0400

    Adds basic functional compontent VideoList
    Lesson 23

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import YTSearch from 'youtube-api-search';
 import { API_KEY } from './api_key';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 class App extends Component {
   constructor(props) {
@@ -21,6 +22,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
