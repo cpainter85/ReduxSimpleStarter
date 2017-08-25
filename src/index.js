4186391659349f52a29686f8aa4469f75db24dc4
Charles Painter <cpainter@eenews.net>
2017-08-25 15:54:56 -0400

    Initializes with default search
    Lesson 22

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import { API_KEY } from './api_key';
 
 import SearchBar from './components/search_bar';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = { videos:[] };
+    
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos }); // equivalent to this.setState({ videos: videos }), only works when key and value variable names are the same
+    });
+  }
+
   render() {
     return (
       <div>
