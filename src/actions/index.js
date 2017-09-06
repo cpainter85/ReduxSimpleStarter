ce530ac0c9390dbd2c480edee245a12332988a7b
Charles Painter <cpainter@eenews.net>
2017-09-06 17:13:54 -0400

    revises selectBook to return an action
    Lesson 46

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,9 @@
 export function selectBook(book) {
-  console.log('A book has been selected:', book.title);
+  // selectBook is an ActionCreator
+  // it needs to return an action, an object with a type property.
+  // an action always contains a type and sometimes a payload
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
